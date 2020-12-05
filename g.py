0d630217eac5d8bdd8a3646a556bef15ac1f17b4
Asmin Silwal <asmin.076bei040@pcampus.edu.np>
2020-12-05 09:00:16 +0545

    change in g.py (c++20 changed to c++2a)

--- g.py
+++ g.py
@@ -2,4 +2,4 @@
 
 import os
 
-os.system("g++ -std=c++20  main.cpp StateMachine.cpp  Game.cpp SplashScreen.cpp MenuScreen.cpp GameState.cpp TimeManager.cpp PausedState.cpp HalfTime.cpp GameOver.cpp -o main -lsfml-graphics -lsfml-system -lsfml-window")
+os.system("g++ -std=c++2a  main.cpp StateMachine.cpp  Game.cpp SplashScreen.cpp MenuScreen.cpp GameState.cpp TimeManager.cpp PausedState.cpp HalfTime.cpp GameOver.cpp -o main -lsfml-graphics -lsfml-system -lsfml-window")
